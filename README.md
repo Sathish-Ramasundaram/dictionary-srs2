7435f5adf408442335d9298add72d6bf2bf95594
Sathi <sathi@example.com>
2026-01-18 20:08:28 +0530

    word

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 "# dictionary-srs2" 
+
+
+
+
+Google sheet URL - 
+https://docs.google.com/spreadsheets/d/1_SCEFy8J8tE5AYpEIHbcDnHD3584_yOtXowDLaXAut8/edit?gid=0#gid=0
+
+Needed part
+1_SCEFy8J8tE5AYpEIHbcDnHD3584_yOtXowDLaXAut8
+
+https://opensheet.elk.sh/1_SCEFy8J8tE5AYpEIHbcDnHD3584_yOtXowDLaXAut8/Sheet1
+
+JSON - .../sheettabname
+
---
+++ script.js
@@ -0,0 +1,27 @@
+window.words = [];
+
+
+const SHEET_URL ="https://opensheet.elk.sh/1_SCEFy8J8tE5AYpEIHbcDnHD3584_yOtXowDLaXAut8/Sheet1";
+
+fetch(SHEET_URL)
+  .then(res => res.json())
+  .then(data => window.words = data);
+
+searchInput.addEventListener("keydown", e => {
+    if (e.key === "Enter") {
+        const found = words.find(
+        w => w.English.toLowerCase() === searchInput.value.toLowerCase()
+    );
+    result.innerHTML = found ? 
+    `<div>
+    <h3>${found.English}</h3>
+
+
+    <p><strong>Meaning:</strong> ${found.Meaning}</p>
+    <p><strong>Tamil:</strong> ${found.Tamil}</p>
+    <p><strong>Kannada:</strong> ${found.Kannada}</p>
+    <p><strong>Hindi:</strong> ${found.Hindi}</p>
+    
+    </div>` : "<p>Word not found</p>";
+    }
+});
